61f94997f4150e7a4de2345b8a4ca19674f6be83
Guilherme <guilherme.dds.dev@gmail.com>
2025-10-28 15:25:44 -0300

    Atualizar nav e adicionar titulo header

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,24 +1,29 @@
-import background from "../assets/background.png";
 import { Link } from "react-router-dom";
 import styles from "../pages/Home.module.css";
+import logo from "../assets/logo.png";
 
 function Home() {
   return (
-    <div>
-      <div className={styles.background}>
-        <img src={background} alt="" />
-      </div>
-      <div className={styles.navBackground}>
-        <div className={styles.nav}>
-          <div className={styles.action}>
-            <Link to="/login" className={styles.login}>
-              Login
-            </Link>
-            <Link to="/signup" className={styles.signup}>
-              Cadastre-se
-            </Link>
-          </div>
+    <div className={styles.homeContainer}>
+      <div className={styles.navContainer}>
+        <img src={logo} className={styles.logo} />
+        <ul className={styles.navSections}>
+          <li>Principais Feiras</li>
+          <li>Sec 2</li>
+          <li>Sec 3</li>
+        </ul>
+        <div className={styles.action}>
+          <Link to="/login" className={styles.login}>
+            Login
+          </Link>
+          <Link to="/signup" className={styles.signup}>
+            Cadastre-se
+          </Link>
         </div>
+      </div>
+      <div className={styles.header}>
+        <h1>ENCONTRE OS LUGARES CERTOS</h1>
+        <h2>PARA O SEU PROJETO</h2>
       </div>
     </div>
   );
