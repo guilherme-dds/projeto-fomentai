bd23492a32c25b2c480237feacd5ebc8a7608d39
Guilherme <guilherme.dds.dev@gmail.com>
2025-10-29 13:23:58 -0300

    Adicionar area de descrição do projeto

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import { Upload } from "lucide-react";
 import { Link } from "react-router-dom";
-import styles from "../pages/Home.module.css";
+import styles from "./Home.module.css";
 import logo from "../assets/logo.png";
 
 function Home() {
+  const [fileName, setFileName] = useState(null);
+  const [text, setText] = useState("");
+  const maxLength = 2000;
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFileName(file.name);
+    } else {
+      setFileName(null);
+    }
+  };
+
   return (
     <div className={styles.homeContainer}>
       <div className={styles.navContainer}>
@@ -24,6 +39,48 @@ function Home() {
       <div className={styles.header}>
         <h1>ENCONTRE OS LUGARES CERTOS</h1>
         <h2>PARA O SEU PROJETO</h2>
+        <p>
+          Descubra, em poucos segundos, quais feiras combinam com o seu <br />{" "}
+          projeto e aumente suas chances de <span>sucesso</span> e{" "}
+          <span>reconhecimento</span>.
+        </p>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.textArea}>
+          <textarea
+            value={text}
+            maxLength={maxLength}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Descreva o seu projeto aqui"
+            className={styles.projectDescription}
+          />
+        </div>
+        <div className={styles.contentInfo}>
+          <p className={styles.fileNameDisplay}>
+            {fileName && <span>{fileName}</span>}
+          </p>
+          <span className={styles.couter}>
+            {text.length}/{maxLength}
+          </span>
+        </div>
+        <div className={styles.btnContent}>
+          <div className={styles.uploadContainer}>
+            <label htmlFor="pdf-upload" className={styles.uploadLabel}>
+              <Upload size={20} />
+              <span>UPLOAD</span>
+            </label>
+            <input
+              id="pdf-upload"
+              type="file"
+              accept=".pdf"
+              className={styles.hiddenInput}
+              onChange={handleFileChange}
+            />
+          </div>
+          <button className={styles.btnFind}>
+            ENCONTRE AS MELHORES FEIRAS
+          </button>
+        </div>
       </div>
     </div>
   );
